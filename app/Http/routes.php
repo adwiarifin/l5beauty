5ca204899777ef1b90c8ca9d33c06b81c465e676
Adwi Arifin <adwiarifin@gmail.com>
2018-03-07 09:15:23 +0700

    make route and controller for admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,8 +1,26 @@
 <?php
 
+// Blog pages
 get('/', function () {
     return redirect('/blog');
 });
-
 get('blog', 'BlogController@index');
 get('blog/{slug}', 'BlogCOntroller@showPost');
+
+// Admin area
+get('admin', function() {
+    return redirect('/admin/post');
+});
+$router->group([
+    'namespace' => 'Admin',
+    'middleware' => 'auth'
+], function() {
+    resource('admin/post', 'PostController');
+    resource('admin/tag', 'TagController');
+    get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+get('/login', 'Auth\AuthController@getLogin');
+post('/login', 'Auth\AuthController@postLogin');
+get('logout', 'Auth\AuthController@getLogout');
